a2cc5452a849330e5bdcb66e818b9058381fde0e
San1aClaus <yantoelsha@gmail.com>
2023-07-11 17:28:21 +0800

    kaya q na mag-commit (sa github)

---
+++ index.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let li = document.querySelectorAll(".faq-text li");
+    for (var i = 0; i < li.length; i++) {
+      li[i].addEventListener("click", function (e) {
+        let clickedLi;
+        if (e.target.classList.contains("question-arrow")) {
+          clickedLi = e.target.parentElement;
+        } else {
+          clickedLi = e.target.parentElement.parentElement;
+        }
+        clickedLi.classList.toggle("showAnswer");
+      });
+    }
+  });
+  
